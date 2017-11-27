0cdc80955f02410f6eb5f365dc98003bf19cbe7f
Kuba <jakubryszczyk.inf@gmail.com>
2017-11-27 20:17:45 +0100

    Chyba zrobione

--- BO projekt/main.py
+++ BO projekt/main.py
@@ -8,14 +8,6 @@ distanceMatrix = bo.openCsvFile('tabelaOdleglosci.csv')
 goods = bo.generateGoodsList(112)
 priority = bo.generatePriorityList(112)
 
-#exSol = bo.generateExampleSolution(10, 2, goods)
-
-#exSol = np.array([[2,3,5],[4,3,5]])
-
-#print(exSol)
-
-#print(bo.mutate(exSol,goods))
-
 plotData = bo.doMagic(100,50, distanceMatrix, goods, priority)
 
 plt.plot(plotData)
