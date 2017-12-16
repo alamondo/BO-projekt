f85ff1b72c3c40ff9895d5fe05201489950876c9
Kuba <jakubryszczyk.inf@gmail.com>
2017-12-16 11:43:45 +0100

    testy + wyswietlanie wynikow

--- BO projekt/testModule.py
+++ BO projekt/testModule.py
@@ -2,21 +2,43 @@ import BOpack as bo
 import random
 import matplotlib.pyplot as plt
 import numpy as np
+import os
 
-def test(a,b):
+def test(vectorOfIterNum,vectorOfPopulationSizes,vectorOfCrossChances,randomSeed):
+# wykonanie algorytmu dla wszystkich kombinacji wartosci w wektorach
 
-    random.seed(None)
+    numberOfThingsToCheck = len(vectorOfCrossChances) + len(vectorOfPopulationSizes) + len(vectorOfIterNum)
+    thingsChecked = 1
+
+    random.seed(randomSeed)
     distanceMatrix = bo.openCsvFile('tabelaOdleglosci.csv')
     goods = bo.generateGoodsList(112)
     priority = bo.generatePriorityList(112)
-    plotData = []
-    for i in range(2,a):
-        for j in range(2,b):
-            print('dupa')
-            plotData.append(bo.doMagic(i, j, distanceMatrix, goods, priority))
-            #plt.plot(plotData)
-            #@plt.show()
+    data = []
 
-    for each in plotData:
-        plt.plot(each)
-        plt.show()
+    for eachIterNum in vectorOfIterNum:
+        for eachPopSize in vectorOfPopulationSizes:
+            for eachChance in vectorOfCrossChances:
+                clear = lambda: os.system('cls')
+                clear()
+                print(' ',np.int16(100 * thingsChecked / numberOfThingsToCheck), '%')
+                thingsChecked += 1
+                data.append([
+                    bo.doMagic(eachIterNum,eachPopSize,eachChance,distanceMatrix,goods,priority)[-1],
+                    eachIterNum,
+                    eachPopSize,
+                    eachChance
+                ])
+
+    return data
+
+def showTestResults(testResultsMatrix):
+# prezentowanie wynikow testow
+# ultra biedne
+# TODO zapis do pliku w celu odczytania w Matlabie
+
+    i = 1
+    for each in testResultsMatrix:
+        print('test#:',i)
+        print('result:',np.int16(each[0]),'\nnum of interations: ',each[1],'\npopulation size:    ',each[2],'\nchance of crossover:',each[3],'\n')
+        i += 1
