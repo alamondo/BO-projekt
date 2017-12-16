2c4abdd483f33ffb28a9d43edeb912ecf86bafb8
Kuba <jakubryszczyk.inf@gmail.com>
2017-12-16 11:44:39 +0100

    testy + wyswietlanie wynikow

--- BO projekt/main.py
+++ BO projekt/main.py
@@ -1,6 +1,35 @@
 import BOpack as bo
 import matplotlib.pyplot as plt
 import testModule
-#import numpy as np'''
+import random
 
-testModule.test(10,10)
+# TODO jakies inne mutowanie
+# TODO jakies inne krzyzowanie
+# TODO testy / dodanie zapisu wynikow do pliku
+
+temp = testModule.test([20],[20],[0,10,20,30,40,50,60,70,80,90,100],None)
+
+testModule.showTestResults(temp)
+
+'''
+random.seed(12)
+distanceMatrix = bo.openCsvFile('tabelaOdleglosci.csv')
+goods = bo.generateGoodsList(112)
+priority = bo.generatePriorityList(112)
+
+
+exSol1 = bo.generateExampleSolution(10,10,goods)
+exSol2 = bo.generateExampleSolution(10,10,goods)
+
+
+cross = bo.crossover(exSol1,exSol2)
+
+print('genom 1:\n',bo.prepareSolution(exSol1),'\ngenom 2:\n',bo.prepareSolution(exSol2),'\nwynik krzyzowania:\n',cross)
+
+
+plotData = bo.doMagic(50, 50, 30, distanceMatrix, goods, priority)
+
+plt.plot(plotData)
+plt.title('temp')
+plt.show()
+'''
